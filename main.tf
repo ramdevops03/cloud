1e33f174e31fdc99c375237b7dd54303570c232a
ramdevops03 <79424528+ramdevops03@users.noreply.github.com>
2022-06-04 18:54:15 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,40 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "sample-session"
-  location = "West Europe"
+data "azurerm_resource_group" "example" {
+   name     = "session"
+   location = "East US 2"
 }
+
+# resource "azurerm_app_service_plan" "example" {
+#   name                = "session-asp"
+#   location            = ""
+#   resource_group_name = azurerm_resource_group.example.name
+
+#   sku {
+#     tier = "Standard"
+#     size = "S1"
+#   }
+# }
+
+# resource "azurerm_app_service" "example" {
+#   name                = "example-app-service"
+#   location            = azurerm_resource_group.example.location
+#   resource_group_name = azurerm_resource_group.example.name
+#   app_service_plan_id = azurerm_app_service_plan.example.id
+
+#   site_config {
+#     dotnet_framework_version = "v4.0"
+#     scm_type                 = "LocalGit"
+#   }
+
+#   app_settings = {
+#     "SOME_KEY" = "some-value"
+#   }
+
+#   connection_string {
+#     name  = "Database"
+#     type  = "SQLServer"
+#     value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
+#   }
+# }
