1bcf8ff7d62c94939e503206278e4b5a06a774c3
ramdevops03 <79424528+ramdevops03@users.noreply.github.com>
2022-06-04 14:54:45 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.9.0"
-    }
-  }
-}
+#terraform {
+#  required_providers {
+#    azurerm = {
+#      source = "hashicorp/azurerm"
+#      version = "3.9.0"
+#    }
+#  }
+#}
 
 provider "azurerm" {
   skip_provider_registration = "true"
