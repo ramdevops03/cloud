ffc7c84a8bb74aaecc58055577d1dacd9af02414
ramdevops03 <79424528+ramdevops03@users.noreply.github.com>
2022-06-04 22:53:49 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "azurerm" {
 
 data "azurerm_resource_group" "example" {
    name     = "session"
-   location = "eastus2"
+   #location = "eastus2"
 }
 
 # resource "azurerm_app_service_plan" "example" {
